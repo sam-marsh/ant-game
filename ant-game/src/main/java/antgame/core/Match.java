26590a8c614f5859cc8233ca0e54c84d8d033bf8
Sam <samuel.marsh@bigpond.com>
2016-05-02 21:56:02 +0100

    make a few changes

--- ant-game/src/main/java/antgame/core/Match.java
+++ ant-game/src/main/java/antgame/core/Match.java
@@ -7,6 +7,9 @@ import antgame.core.Colony.Colour;
 import antgame.core.world.*;
 import antgame.core.world.Cell.Type;
 
+import java.util.Set;
+import java.util.stream.Collectors;
+
 /**
  *
  * @author Regan
@@ -16,7 +19,7 @@ public class Match {
     private final Player playerRed;
     private final Player playerBlack;
     private final World world;
-    
+
     public Match(Player playerRed, Player playerBlack, World world)
     {
         this.playerRed = playerRed;
@@ -24,53 +27,55 @@ public class Match {
         this.world = world;
     }
     
-    public Player run(int rounds)
+    public MatchOutcome run(int rounds)
     {
         int redFood = 0;
         int blackFood = 0;
         
         world.spawnAnts(new Colony(Colour.RED, playerRed.getBrain()), new Colony(Colour.RED, playerBlack.getBrain()));
-        for (int i = 0; i<rounds  ;i++)
+        for (int i = 0; i < rounds; i++)
         {
-            for (Ant ant : world.getAnts())
-            {
-                if (ant.surrounded())
-                {
-                    world.murder(ant);
-                }
-            }
-            
-            for (Ant ant : world.getAnts())
-            {    
-                ant.step();
-            }
+            Set<Ant> toRemove = world.getAnts().stream().filter(Ant::surrounded).collect(Collectors.toSet());
+            toRemove.stream().forEach(world::murder);
+
+            world.getAnts().forEach(Ant::step);
         }
         
-        for (int x = 0; x<=world.width(); x++)
+        for (int x = 0; x < world.width(); x++)
         {
-            for (int y = 0; y<=world.height(); y++)
+            for (int y = 0; y < world.height(); y++)
             {
+                Cell curr = world.cell(x, y);
                 if (world.cell(x, y).getType() == Type.ANTHILL_RED)
                 {
-                    redFood++;
+                    redFood += curr.getFoodAmount();
                 }
                 else if (world.cell(x, y).getType() == Type.ANTHILL_BLACK)
                 {
-                    blackFood++;
+                    blackFood += curr.getFoodAmount();
                 }
             }
         }
-        
-        if (redFood > blackFood)
-        {
-            return playerRed;
+
+        if (redFood > blackFood) {
+            return new MatchOutcome(
+                    new PlayerOutcome(playerRed, PlayerOutcome.Result.WIN, this),
+                    new PlayerOutcome(playerBlack, PlayerOutcome.Result.LOSS, this)
+            );
+        } else if (blackFood > redFood) {
+            return new MatchOutcome(
+                    new PlayerOutcome(playerRed, PlayerOutcome.Result.LOSS, this),
+                    new PlayerOutcome(playerBlack, PlayerOutcome.Result.WIN, this)
+            );
+        } else {
+            return new MatchOutcome(
+                    new PlayerOutcome(playerRed, PlayerOutcome.Result.DRAW, this),
+                    new PlayerOutcome(playerBlack, PlayerOutcome.Result.DRAW, this)
+            );
         }
-        else
-        {
-            return playerBlack;
-        }
+
     }
-    
+
     public World world() {
         return world;
     }
