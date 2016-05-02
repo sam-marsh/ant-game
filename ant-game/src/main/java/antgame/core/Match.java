3b8075ffb852a36ec34857c2be33ebb117b9bbaa
Regan <Regan@Regan-Gryphon>
2016-05-02 23:01:15 +0100

    Added Match documentation

--- ant-game/src/main/java/antgame/core/Match.java
+++ ant-game/src/main/java/antgame/core/Match.java
@@ -1,6 +1,3 @@
-/*
- * A match between two supplied players
- */
 package antgame.core;
 
 import antgame.core.Colony.Colour;
@@ -11,17 +8,28 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 /**
- *
- * @author Regan
+ * A match between two supplied players.
+ * Once match has been constructed call run() which returns the winning player.
+ * @author Regan Ware
  */
 public class Match {
 
     public static int NUM_ROUNDS = 300000;
 
+    //The player to be assigned to the red colony within the match
     private final Player playerRed;
+    //The player to be assigned to the black colony within the match
     private final Player playerBlack;
+    //The world this match takes place in
     private final World world;
 
+    /**
+     * Creates a new Match using the supplied players and world
+     *
+     * @param playerRed The player who's brain will control the red colony
+     * @param playerBlack The player who's brain will control the black colony
+     * @param world The parsed or generated world the match will be played in
+     */
     public Match(Player playerRed, Player playerBlack, World world)
     {
         this.playerRed = playerRed;
@@ -29,6 +37,12 @@ public class Match {
         this.world = world;
     }
     
+    /**
+     * Runs the match determining and returning the winner based on which colony has more food in their ant hills
+     *
+     * @param rounds The number of rounds to run the match for (Usually NUM_ROUNDS at 300000)
+     * @return The outcome (LOSS, WIN, DRAW) of the match
+     */
     public MatchOutcome run(int rounds, int speed)
     {
         int redFood = 0;
@@ -82,6 +96,9 @@ public class Match {
 
     }
 
+    /**
+     * @return The world this match takes place in
+     */
     public World world() {
         return world;
     }
