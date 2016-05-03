1423e143d21cda134adde6868f35008a8c761aca
Regan <rw277@sussex.ac.uk>
2016-05-03 14:42:47 +0100

    Documented Match.java

--- ant-game/src/main/java/antgame/core/Player.java
+++ ant-game/src/main/java/antgame/core/Player.java
@@ -8,37 +8,67 @@ package antgame.core;
 import antgame.core.brain.Brain;
 
 /**
+ * A player to participate in a match or tournament. Has a name for GUI, points for tournaments and a brain to manage a colony within a Match
  *
  * @author Regan
  */
 public class Player implements Comparable<Player> {
 
+    //Player's display name
     private final String name;
+    //The ant brain that will run an assigned colony
     private final Brain  brain;
+    //The number of points won by this player
     private int points;
 
+    /**
+     * Creates a new Player with the display name and brain supplied
+     *
+     * @param name The desired display name of the player
+     * @param brain The parsed brain to control this player's colonies
+     */
     public Player(String name, Brain brain)
     {
         this.name = name;
         points = 0;
         this.brain = brain;
     }
-    
+
+    /**
+     * Creates a new Match using the supplied players and world
+     *
+     * @param points The number to points to add to the player's current points
+     */
     public void addPoints(int points)
     {
         this.points += points;
     }
-    
+
+    /**
+     * Creates a new Match using the supplied players and world
+     *
+     * @return The parsed brain of this player
+     */
     public Brain getBrain()
     {
         return brain; 
     }
 
+    /**
+     * Creates a new Match using the supplied players and world
+     *
+     * @return The difference between the two player's points
+     */
     @Override
     public int compareTo(Player o) {
         return points - o.points;
     }
 
+    /**
+     * Creates a new Match using the supplied players and world
+     *
+     * @return The number of points this player has
+     */
     public int points() {
         return points;
     }
